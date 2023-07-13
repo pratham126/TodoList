2f788380228969276c7e53e6054ad47cc194d9aa
Pratham <prathamchawlaarora465@gmail.com>
2023-07-13 11:51:58 +0530

    testing useeffect in prod 1

--- smart_todo/src/components/HomeScreen.js
+++ smart_todo/src/components/HomeScreen.js
@@ -5,13 +5,17 @@ import axios from 'axios';
 const HomeScreen = () => {
   const [list, setList] = useState([]);
   useEffect(() => {
+    console.log('fetching data now 1');
     const addData = async () => {
+      console.log('fetching data now 2');
       const { data } = await axios.get(
         'https://todolist-backend-560u.onrender.com/items'
       );
       setList(data.items.sort((a, b) => new Date(b.date) - new Date(a.date)));
+      console.log('fetched the data 1');
     };
     addData();
+    console.log('fetched the data 2');
   }, []);
   const fetchData = async () => {
     const { data } = await axios.get(
